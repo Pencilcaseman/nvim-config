9037742098f320e4a32c2de8bbd0ce32a3cd44b6
Toby Davis <pencilcaseman@gmail.com>
2024-09-12 15:43:58 +0200

    Toggle term commands

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -4,5 +4,47 @@ return {
   opts = {--[[ things you want to change go here]]
     -- Shell is $HOME/.nix-profile/bin/fish
     shell = vim.env.HOME .. '/.nix-profile/bin/fish',
+    size = function(term)
+      if term.direction == 'horizontal' then
+        return 15
+      elseif term.direction == 'vertical' then
+        return vim.o.columns * 0.4
+      end
+      return vim.o.lines * 0.4
+    end,
   },
+  keys = function(_, keys)
+    local toggleterm = require 'toggleterm'
+    return {
+      {
+        '<leader>ttf',
+        function()
+          toggleterm.toggle(nil, nil, nil, 'float')
+        end,
+        desc = '[T]oggle [T]erminal [F]loat',
+      },
+      -- {
+      --   '<leader>ttb',
+      --   function()
+      --     toggleterm.toggle(nil, nil, nil, 'tab')
+      --   end,
+      --   desc = '[T]oggle [T]erminal [T]ab',
+      -- },
+      {
+        '<leader>ttv',
+        function()
+          toggleterm.toggle(nil, nil, nil, 'vertical')
+        end,
+        desc = '[T]oggle [T]erminal [V]ertical',
+      },
+      {
+        '<leader>tth',
+        function()
+          toggleterm.toggle(nil, nil, nil, 'horizontal')
+        end,
+        desc = '[T]oggle [T]erminal [H]orizontal',
+      },
+      unpack(keys),
+    }
+  end,
 }
