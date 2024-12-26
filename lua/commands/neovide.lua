195c977745e067455ee805ebcd247fe0df81aca4
Toby Davis <pencilcaseman@gmail.com>
2024-12-26 17:57:23 +0000

    Update font size

--- lua/commands/neovide.lua
+++ lua/commands/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_hide_mouse_when_typing = true
-  vim.opt.guifont = { 'MonaSpiceNe Nerd Font', ':h13' }
+  vim.opt.guifont = { 'MonaSpiceNe Nerd Font', ':h11' }
 end
