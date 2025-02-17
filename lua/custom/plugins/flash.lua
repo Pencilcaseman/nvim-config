708306bcd967b3f5605f01d40e80855362b78d32
Toby Davis <pencilcaseman@gmail.com>
2025-02-17 01:24:25 +0000

    Tweaks

--- lua/custom/plugins/flash.lua
+++ lua/custom/plugins/flash.lua
@@ -9,6 +9,23 @@ return {
     { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    -- { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
   },
 }
+
+-- return {
+--   'ggandor/leap.nvim',
+--   config = function()
+--     -- require('leap').create_default_mappings()
+--   end,
+--   keys = function()
+--     return {
+--       {
+--         's',
+--         mode = { 'n', 'x', 'o' },
+--         '<Plug>(leap)',
+--         desc = '',
+--       },
+--     }
+--   end,
+-- }
