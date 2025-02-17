bc9d8b250a3820bd0db023d1ff872be9cfe2ee77
Toby Davis <pencilcaseman@gmail.com>
2025-02-17 01:41:45 +0000

    More changes

--- lua/custom/plugins/surround.lua
+++ lua/custom/plugins/surround.lua
@@ -1,10 +1,26 @@
 return {
   'kylechui/nvim-surround',
   event = 'VeryLazy',
-  opts = {},
+  opts = {
+    keymaps = {
+      -- insert = '<C-g>s',
+      -- insert_line = '<C-g>S',
+      normal = 'gs',
+      normal_cur = 'gSs',
+      normal_line = 'gS',
+      normal_cur_line = 'gSS',
+      visual = 'g',
+      visual_line = 'gS',
+      delete = 'gsd',
+      change = 'gsc',
+      change_line = 'gsC',
+    },
+  },
   -- config = function(_, opts)
   --   require('nvim-surround').setup {
   --     -- Configuration here, or leave empty to use defaults
   --   }
   -- end,
 }
+
+-- Sample Line
