b5acb46b5076fa0853135dd325ff911f95a69a66
Toby Davis <pencilcaseman@gmail.com>
2025-02-22 22:33:04 +0000

    Theme changes

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,8 +1,3 @@
--- return {
---   require 'themes.material',
---   require 'themes.onedark',
--- }
-
 return {
   'cpea2506/one_monokai.nvim',
   opts = {
@@ -10,5 +5,8 @@ return {
   },
   config = function(_, opts)
     require('one_monokai').setup(opts)
+
+    vim.cmd [[highlight FlashLabel guibg=#75FE40 guifg=#202060 gui=bold]]
+    vim.cmd [[highlight CursorLine guibg=#282A2E]]
   end,
 }
