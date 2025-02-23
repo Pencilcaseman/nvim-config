8c7e04bd82ce9dabdd40b40b9e831023bcb766f3
Toby Davis <pencilcaseman@gmail.com>
2025-02-23 20:32:12 +0000

    Cleanup init.lua

--- init.lua
+++ init.lua
@@ -1,18 +1,12 @@
 if vim.env.PROF then
-  -- example for lazy.nvim
-  -- change this to the correct path for your plugin manager
   local snacks = vim.fn.stdpath 'data' .. '/lazy/snacks.nvim'
   vim.opt.rtp:append(snacks)
   require('snacks.profiler').startup {
     startup = {
-      event = 'VimEnter', -- stop profiler on this event. Defaults to `VimEnter`
-      -- event = "UIEnter",
-      -- event = "VeryLazy",
+      event = 'VimEnter',
     },
   }
 end
-
--- vim.opt.showtabline = 2 -- Always reserve space for tabline
 
 vim.opt.showtabline = 2 -- Always reserve space for the tabline (barbar)
 vim.opt.winbar = 'NeoVim > Bread > Crumbs' -- Preload space for barbecue
