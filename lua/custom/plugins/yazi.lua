04bbb3e777562739a4df956731f0dc3062fb95bb
Toby Davis <pencilcaseman@gmail.com>
2025-02-23 20:32:59 +0000

    Cleanup

--- lua/custom/plugins/yazi.lua
+++ lua/custom/plugins/yazi.lua
@@ -19,8 +19,6 @@ return {
         desc = "Open the file manager in nvim's working directory",
       },
       {
-        -- NOTE: this requires a version of yazi that includes
-        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
         '<leader>fr',
         '<cmd>Yazi toggle<cr>',
         desc = 'Resume the last yazi session',
@@ -29,7 +27,6 @@ return {
 
     ---@type YaziConfig
     opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
       open_for_directories = true,
       keymaps = {
         show_help = '<f1>',
