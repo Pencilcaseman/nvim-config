01116d31a8d7d270a1b7d6acdf7d0f3ec6ec88e3
Toby Davis <pencilcaseman@gmail.com>
2025-02-23 07:35:40 +0000

    Why no worky?

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -1,3 +1,11 @@
+return {
+  'lewis6991/gitsigns.nvim',
+  opts = {},
+  config = function(_, opts)
+    require('gitsigns').setup(opts)
+  end,
+}
+
 -- return {
 --   'lewis6991/gitsigns.nvim',
 --   event = 'VeryLazy',
@@ -72,77 +80,3 @@
 --     map('n', '<leader>gd', gitsigns.toggle_deleted, { desc = '[G]it Show [D]eleted' })
 --   end,
 -- }
-
-return {
-  'lewis6991/gitsigns.nvim',
-  event = 'VeryLazy',
-  opts = {
-    signs = {
-      add = { text = '┃' },
-      change = { text = '┃' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-      untracked = { text = '┆' },
-    },
-
-    signs_staged = {
-      enable = true, -- Corrected key
-      add = { text = '┃' },
-      change = { text = '┃' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-      untracked = { text = '┆' },
-    },
-
-    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-    watch_gitdir = {
-      follow_files = true,
-    },
-
-    auto_attach = true,
-    attach_to_untracked = false,
-    current_line_blame = false,
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-      delay = 1000,
-      ignore_whitespace = false,
-      virt_text_priority = 100,
-      use_focus = true,
-    },
-
-    current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil, -- Use default
-    max_file_length = 40000, -- Disable if file is longer than this (in lines)
-    preview_config = {
-      border = 'single',
-      style = 'minimal',
-      relative = 'cursor',
-      row = 0,
-      col = 1,
-    },
-  },
-
-  config = function(_, opts)
-    require('gitsigns').setup(opts)
-
-    local gitsigns = require 'gitsigns'
-
-    local function map(mode, l, r, map_opts)
-      map_opts = map_opts or {}
-      map_opts.buffer = vim.api.nvim_get_current_buf()
-      vim.keymap.set(mode, l, r, map_opts)
-    end
-
-    map('n', '<leader>gb', gitsigns.blame_line, { desc = '[G]it [B]lame line' })
-    map('n', '<leader>gt', gitsigns.toggle_current_line_blame, { desc = '[G]it [T]oggle Blame' })
-    map('n', '<leader>gd', gitsigns.toggle_deleted, { desc = '[G]it Show [D]eleted' })
-  end,
-}
