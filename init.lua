c6ec35a935d8b3a93ed8d028575792f7b532a1a3
Toby Davis <pencilcaseman@gmail.com>
2025-02-26 04:51:54 +0000

    Enable spellchecking

--- init.lua
+++ init.lua
@@ -12,17 +12,22 @@ if vim.env.PROF then
   }
 end
 
-vim.opt.showtabline = 2 -- Always reserve space for the tabline (barbar)
-vim.opt.winbar = 'NeoVim > Bread > Crumbs' -- Preload space for barbecue
+-- Set this to false if you do not have a NerdFont installed
+vim.g.have_nerd_font = true
 
 -- Set <space> as the leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Enable spell checking
+vim.opt.spell = true
+vim.opt.spelllang = 'en'
+
+vim.opt.showtabline = 2 -- Always reserve space for the tabline (barbar)
+vim.opt.winbar = 'NeoVim > Bread > Crumbs' -- Preload space for barbecue
+
 vim.g.loaded_netrw = true
 vim.g.loaded_netrwPlugin = true
-
-vim.g.have_nerd_font = true
 
 require 'options'
 require 'keymaps'
