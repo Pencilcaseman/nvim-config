3ec539d0ba7ec794207489ab46422d6347d72394
Toby Davis <pencilcaseman@gmail.com>
2025-03-16 21:04:30 +0000

    Barbar black sheep

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,7 +1,12 @@
 return {
   'stevearc/aerial.nvim',
-  opts = {},
-  -- Optional dependencies
+  opts = {
+    layout = {
+      max_width = { 40, 0.2 },
+      min_width = { 40, 0.2 },
+    },
+  },
+
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'nvim-tree/nvim-web-devicons',
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -9,9 +9,24 @@ return {
     vim.cmd [[highlight LineNrAbove guifg=#516C96]]
     vim.cmd [[highlight LineNrBelow guifg=#516C96]]
 
+    vim.cmd [[highlight BufferCurrentCHANGED guibg=#222436 guifg=#6B71F2]]
+    vim.cmd [[highlight BufferCurrentDELETED guibg=#222436 guifg=#E55444]]
+    vim.cmd [[highlight BufferCurrentADDED guibg=#222436 guifg=#43E661]]
+
+    vim.cmd [[highlight BufferVisibleCHANGED guibg=#282A3F guifg=#6B71F2]]
+    vim.cmd [[highlight BufferVisibleDELETED guibg=#282A3F guifg=#E55444]]
+    vim.cmd [[highlight BufferVisibleADDED guibg=#282A3F guifg=#43E661]]
+
+    vim.cmd [[highlight BufferInactiveCHANGED guibg=#282A3F guifg=#6B71F2]]
+    vim.cmd [[highlight BufferInactiveDELETED guibg=#282A3F guifg=#E55444]]
+    vim.cmd [[highlight BufferInactiveADDED guibg=#282A3F guifg=#43E661]]
+
+    vim.cmd [[highlight BufferAlternateCHANGED guibg=#282A3F guifg=#6B71F2]]
+    vim.cmd [[highlight BufferAlternateDELETED guibg=#282A3F guifg=#E55444]]
+    vim.cmd [[highlight BufferAlternateADDED guibg=#282A3F guifg=#43E661]]
+
     if vim.g.neovide then
       -- This is a comment
-      -- vim.cmd [[ highlight Comment guifont="Monaspace Radon Var"]]
       vim.opt.guifont = 'Monaspace Radon Var'
     end
   end,
