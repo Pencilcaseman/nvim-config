a3596cd49fcbd59c65fa75f4e128a07e05be6a95
Toby Davis <pencilcaseman@gmail.com>
2025-03-18 15:09:22 +0000

    Re-add git status to dashbaord

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -38,20 +38,20 @@ return {
         { section = 'keys', gap = 1, padding = 1 },
         { pane = 2, icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
         { pane = 2, icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
-        -- {
-        --   pane = 2,
-        --   icon = ' ',
-        --   title = 'Git Status',
-        --   section = 'terminal',
-        --   enabled = function()
-        --     return Snacks.git.get_root() ~= nil
-        --   end,
-        --   cmd = 'git status --short --branch --renames',
-        --   height = 10,
-        --   padding = 1,
-        --   ttl = 5 * 60,
-        --   indent = 3,
-        -- },
+        {
+          pane = 2,
+          icon = ' ',
+          title = 'Git Status',
+          section = 'terminal',
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = 'git status --short --branch --renames',
+          height = 10,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
         { section = 'startup' },
       },
     },
