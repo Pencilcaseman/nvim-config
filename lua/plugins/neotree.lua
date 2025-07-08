018d00cc0046a0a46bacabeae67c46de875a8a12
Toby Davis <pencilcaseman@gmail.com>
2025-07-08 08:25:55 +0100

    Neotree reveal current file

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,17 +1,15 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  -- branch = 'v3.x',
 
   dependencies = {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons',
     'MunifTanjim/nui.nvim',
-    -- '3rd/image.nvim',
   },
 
   opts = {},
 
   keys = {
-    { '<leader>e', '<Cmd>Neotree toggle<CR>', desc = 'File [E]xplorer' },
+    { '<leader>e', '<Cmd>Neotree reveal toggle<CR>', desc = 'File [E]xplorer' },
   },
 }
