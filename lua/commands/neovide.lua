341a3f1101f7c759dc8889916463eb612eb05f09
Toby Davis <pencilcaseman@gmail.com>
2025-08-18 23:45:51 +0100

    Update neovide options

--- lua/commands/neovide.lua
+++ lua/commands/neovide.lua
@@ -2,5 +2,5 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_hide_mouse_when_typing = true
   -- vim.opt.guifont = { 'MonaSpiceNe Nerd Font', ':h11' }
-  vim.opt.guifont = { 'Monaspace Neon Var', ':h11' }
+  vim.opt.guifont = { 'Monaspace Neon Var', ':h12' }
 end
