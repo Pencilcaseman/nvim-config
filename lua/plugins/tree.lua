b090bfd916deeb76059bec3fb0bc857cad36c44d
Toby Davis <pencilcaseman@gmail.com>
2025-10-09 21:36:46 +0100

    fix(tree): Downgrade neotree
    See [this PR](https://github.com/nvim-neo-tree/neo-tree.nvim/pull/1838)

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,16 +1,12 @@
--- return {
---   'ms-jpq/chadtree',
---   branch = 'chad',
---   build = 'python3 -m chadtree deps',
---
---   keys = {
---     { '<leader>e', '<cmd>CHADopen<cr>', desc = 'File [E]xplorer' },
---   },
--- }
-
 return {
   {
     'nvim-neo-tree/neo-tree.nvim',
+
+    -- There's an annoying bug after this release
+    -- TODO: Update when fixed
+    tag = '3.35',
+    -- branch = 'v3.x',
+
     dependencies = {
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
@@ -20,33 +16,5 @@ return {
     keys = {
       { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'File [E]xplorer' },
     },
-  },
-  {
-    'antosha417/nvim-lsp-file-operations',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
-    },
-    config = function()
-      require('lsp-file-operations').setup()
-    end,
-  },
-  {
-    's1n7ax/nvim-window-picker',
-    config = function()
-      require('window-picker').setup {
-        filter_rules = {
-          include_current_win = false,
-          autoselect_one = true,
-          -- filter using buffer options
-          bo = {
-            -- if the file type is one of following, the window will be ignored
-            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
-            -- if the buffer type is one of following, the window will be ignored
-            buftype = { 'terminal', 'quickfix' },
-          },
-        },
-      }
-    end,
   },
 }
