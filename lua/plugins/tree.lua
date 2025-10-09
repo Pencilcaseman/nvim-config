0dad5064d473511141a805a72fc4ff93af124e8d
Toby Davis <pencilcaseman@gmail.com>
2025-10-09 21:12:09 +0100

    plugin(tree): Return to neotree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,9 +1,52 @@
-return {
-  'ms-jpq/chadtree',
-  branch = 'chad',
-  build = 'python3 -m chadtree deps',
+-- return {
+--   'ms-jpq/chadtree',
+--   branch = 'chad',
+--   build = 'python3 -m chadtree deps',
+--
+--   keys = {
+--     { '<leader>e', '<cmd>CHADopen<cr>', desc = 'File [E]xplorer' },
+--   },
+-- }
 
-  keys = {
-    { '<leader>e', '<cmd>CHADopen<cr>', desc = 'File [E]xplorer' },
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons',
+    },
+
+    keys = {
+      { '<leader>e', '<cmd>Neotree toggle<cr>', desc = 'File [E]xplorer' },
+    },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
+    },
+    config = function()
+      require('lsp-file-operations').setup()
+    end,
+  },
+  {
+    's1n7ax/nvim-window-picker',
+    config = function()
+      require('window-picker').setup {
+        filter_rules = {
+          include_current_win = false,
+          autoselect_one = true,
+          -- filter using buffer options
+          bo = {
+            -- if the file type is one of following, the window will be ignored
+            filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+            -- if the buffer type is one of following, the window will be ignored
+            buftype = { 'terminal', 'quickfix' },
+          },
+        },
+      }
+    end,
   },
 }
