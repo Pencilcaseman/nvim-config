70cbbe875b52f95a1e6c8895d1a77b39166bbf6b
Toby Davis <pencilcaseman@gmail.com>
2025-10-10 15:51:28 +0100

    update(tree): Latest PR fixed the problem :)

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,11 +2,6 @@ return {
   {
     'nvim-neo-tree/neo-tree.nvim',
 
-    -- There's an annoying bug after this release
-    -- TODO: Update when fixed
-    tag = '3.35',
-    -- branch = 'v3.x',
-
     dependencies = {
       'nvim-lua/plenary.nvim',
       'MunifTanjim/nui.nvim',
